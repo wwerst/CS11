0915e969cfefd3de84ee29cdf8c4b63345617944
wwerst <wwerst@caltech.edu>
2017-05-16 09:38:46 -0700

    Wrote first implementation of maze class

--- CS11_C++/Lab4/maze.cc
+++ CS11_C++/Lab4/maze.cc
@@ -1,151 +1,276 @@
 #include <iostream>
+#include "maze.hh"
 
 using namespace std;
 
 
-// A simple class for representing locations in a 2D array.  The class also
-// implements equality/inequality operators so that we can see if two
-// locations are the same or not.
-class Location {
-public:
-    // The row and column of the location
-    int row;
-    int col;
-
-    // Constructors for initializing locations
-    Location(int row, int col) : row(row), col(col) { }
-    Location() : row(0), col(0) { }
-    
-    // Returns true if this location is the same as the specified location
-    bool operator==(const Location &loc) const {
-        return row == loc.row && col == loc.col;
-    }
-    
-    // Returns true if this location is different from the specified location
-    bool operator!=(const Location &loc) const {
-        return !(*this == loc);
-    }
-};
 
 
-// Possible values for maze cells
-enum class MazeCell {
-    EMPTY,
-    WALL,
-    VISITED
-};
-
-
-// Directions that we can go, relative to a given maze cell
-enum class Direction {
-    NORTH,
-    EAST,
-    SOUTH,
-    WEST
-};
-
-
-class Maze {
-    // The number of rows with cells in them
-    int numRows;
-    
-    // The number of columns with cells in them
-    int numCols;
-    
-    // The maze's "expanded representation"
-    MazeCell *cells;
-
-    // The start of the maze, in cell coordinates
-    Location start;
-    
-    // The end of the maze, in cell coordinates
-    Location end;
-
-public:
     // Initialize a new maze of size rows x cols
-    Maze(int rows, int cols);
+Maze::Maze(int rows, int cols){
+    //Check maze size is valid
+    assert(rows >= 1);
+    assert(cols >= 1);
+    //Initialize the maze variables
+    numRows = rows;
+    numCols = cols;
+    cells = new MazeCell[(2*numRows+1)*(2*numCols+1)];
+}
     
     // Make a copy of an existing maze object
-    Maze(const Maze &m);
+Maze::Maze(const Maze &m){
+    numRows = m.getNumRows();
+    numCols = m.getNumCols();
+    // Generate new location instances for start and end
+    start = Location(m.getStart().row, m.getStart().col);
+    end = Location(m.getEnd().row, m.getEnd().col);
+    // Copy the cells
+    cells = new MazeCell[(2*numRows+1)*(2*numCols+1)];
+    for (int r = 0; r < numRows; r++){
+        for (int c = 0; c < numCols; c++){
+            MazeCell *cell = 
+            cells[getArrayIndex(getCellArrayCoord(r,c))] = m.getCell(r, c);
+            if ()
+        }
+    }
+
+}
     
     // Maze destructor
-    ~Maze();
+Maze::~Maze(){
+    delete [] cells;
+}
     
     // Maze assignment operator
-    Maze & operator=(const Maze &m);
+Maze::Maze & operator=(const Maze &m);
 
 
-    // Returns the number of rows in the maze
-    int getNumRows() const;
+// Returns the number of rows in the maze
+int Maze::getNumRows() const{
+    return numRows;
+}
     
-    // Returns the number of columns in the maze
-    int getNumCols() const;
+// Returns the number of columns in the maze
+int Maze::getNumCols() const{
+    return numCols;
+}
 
 
-    // Returns the starting point in the maze
-    Location getStart() const;
+// Returns the starting point in the maze
+Location Maze::getStart() const{
+    return start;
+}
 
-    // Sets the starting point in the maze    
-    void setStart(int row, int col);
+// Sets the starting point in the maze    
+void Maze::setStart(int row, int col){
+    start = Location(row, col);
+}
     
 
-    // Returns the ending point in the maze    
-    Location getEnd() const;
+// Returns the ending point in the maze    
+Location Maze::getEnd() const{
+    return end;
+}
     
-    // Sets the ending point in the maze
-    void setEnd(int row, int col);
+// Sets the ending point in the maze
+void Maze::setEnd(int row, int col){
+    end = Location(row, col);
+}
     
 
-    // Sets all cells and walls to be empty, so that the maze is
-    // completely cleared
-    void clear();
+// Sets all cells and walls to be empty, so that the maze is
+// completely cleared
+void Maze::clear(){
+    // Index from end of cells array to beginning, clearing all cells.
+    for (int i = (2*numRows+1)*(2*numCols+1) - 1; i >= 0; i--){
+        cells[i] = MazeCell::EMPTY;
+    }
+}
     
-    // Places a wall at every location that can be a wall in the maze
-    void setAllWalls();
+// Places a wall at every location that can be a wall in the maze
+void Maze::setAllWalls(){
+    for (int r = 0; r < numRows - 1; r++){
+        for (int c = 0; c < numCols - 1; c++){
+            cells[getArrayIndex(getWallArrayCoord(r, c, Direction::SOUTH))] = MazeCell::WALL;
+            cells[getArrayIndex(getWallArrayCoord(r, c, Direction::EAST))] = MazeCell::WALL;
+        }
+    }
+}
 
 
-    // Returns the value of the specified
-    MazeCell getCell(int cellRow, int cellCol) const;
+// Returns the value of the specified cell
+MazeCell Maze::getCell(int cellRow, int cellCol) const{
+    assert(cellRow >= 0 && cellRow < numRows);
+    assert(cellCol >= 0 && cellCol < numCols);
+    return cells[getArrayIndex(getCellArrayCoord(cellRow, cellCol))];
+}
 
-    void setCell(int cellRow, int cellCol, MazeCell val);
+void Maze::setCell(int cellRow, int cellCol, MazeCell val){
+    cells[getArrayIndex(getCellArrayCoord(cellRow, cellCol))] = val;
+}
 
-    // Returns the cell-coordinates of the neighboring cell in the specified
-    // direction.  Trips an assertion if the given cell has no neighbor in the
-    // specified direction (e.g. the NORTH neighbor of cell (0,5)).
-    Location getNeighborCell(int cellRow, int cellCol,
-                             Direction direction) const;
+// Returns the cell-coordinates of the neighboring cell in the specified
+// direction.  Trips an assertion if the given cell has no neighbor in the
+// specified direction (e.g. the NORTH neighbor of cell (0,5)).
+Location Maze::getNeighborCell(int cellRow, int cellCol,
+                            Direction direction) const
+{
+    //Check coordinates in maze bounds
+    assert(cellRow >= 0 && cellRow < numRows);
+    assert(cellCol >= 0 && cellCol < numCols);
+    switch direction{
+        case Direction::NORTH:
+            assert(cellRow >= 1);
+            return Location(cellRow - 1, cellCol);
+        case Direction::EAST:
+            assert(cellCol < numCols - 1);
+            return Location(cellRow, cellCol + 1);
+        case Direction::SOUTH:
+            assert(cellRow < numRows - 1);
+            return Location(cellRow + 1, cellCol);
+        case Direction::WEST:
+            assert(cellCol >= 1);
+            return Location(cellRow, cellCol - 1);
+        default:
+            //Should never reach here
+            assert(false);
+    }
+
+}
 
 
-    // Returns true if there is a wall in the specified direction from the
-    // given cell, false otherwise
-    bool hasWall(int cellRow, int cellCol, Direction direction) const;
+// Returns true if there is a wall in the specified direction from the
+// given cell, false otherwise
+bool Maze::hasWall(int cellRow, int cellCol, Direction direction) const{
+    return cells[getArrayIndex(getWallArrayCoord(cellRow, cellCol, direction))] == MazeCell::WALL;
+}
 
-    // Puts a wall on the specified side of the given cell
-    void setWall(int cellRow, int cellCol, Direction direction);
+// Puts a wall on the specified side of the given cell
+void Maze::setWall(int cellRow, int cellCol, Direction direction){
+    cells[getArrayIndex(getWallArrayCoord(cellRow, cellCol, direction))] == MazeCell::WALL;
+}
 
-    // Removes a wall on the specified side of the given cell
-    void clearWall(int cellRow, int cellCol, Direction direction);
-
-
-    // Returns true if the specified maze cell has been visited.
-    bool isVisited(int cellRow, int cellCol) const;
-
-    // Changes the cell's value to VISITED
-    void setVisited(int cellRow, int cellCol);
+// Removes a wall on the specified side of the given cell
+void Maze::clearWall(int cellRow, int cellCol, Direction direction){
+    cells[getArrayIndex(getWallArrayCoord(cellRow, cellCol, direction))] == MazeCell::EMPTY;
+}
 
 
-    // Outputs the maze using simple ASCII-art to the specified output stream.
-    // The output format is as follows, using the example maze from the
-    // assignment write-up.  (The text to the right of the maze is purely
-    // explanatory, and you don't need to output it.)
-    //
-    // 3 4                 (# of rows and then # of columns)
-    // +---+---+---+---+   (each cell is 3 spaces wide, with a + at each corner)
-    // | S     |       |   (walls indicated by --- or |)
-    // +---+   +   +   +   (start indicated by S, end indicated by E)
-    // |   |   |   |   |
-    // +   +   +   +   +
-    // |           | E |
-    // +---+---+---+---+
-    void print(ostream &os) const;
-};
+// Returns true if the specified maze cell has been visited.
+bool Maze::isVisited(int cellRow, int cellCol) const{
+    Cell *cell = cells[getArrayIndex(getWallArrayCoord(cellRow, cellCol, direction))];
+    return cell == MazeCell::VISITED;
+}
+
+// Changes the cell's value to VISITED
+void Maze::setVisited(int cellRow, int cellCol){
+    Cell *cell = cells[getArrayIndex(getWallArrayCoord(cellRow, cellCol, direction))];
+    cell = MazeCell::VISITED;
+}
+
+
+// Outputs the maze using simple ASCII-art to the specified output stream.
+// The output format is as follows, using the example maze from the
+// assignment write-up.  (The text to the right of the maze is purely
+// explanatory, and you don't need to output it.)
+//
+// 3 4                 (# of rows and then # of columns)
+// +---+---+---+---+   (each cell is 3 spaces wide, with a + at each corner)
+// | S     |       |   (walls indicated by --- or |)
+// +---+   +   +   +   (start indicated by S, end indicated by E)
+// |   |   |   |   |
+// +   +   +   +   +
+// |           | E |
+// +---+---+---+---+
+void Maze::print(ostream &os) const{
+    // Output number of rows and columns
+    os << numRows << " " << numCols << endl;
+    // Output the maze
+    for(int r = 0; r < numRows; r++){
+        //Output top walls
+        for (int c = 0; c < numCols; c++){
+            os << "+";
+            if (hasWall(r, c, Direction::NORTH)){
+                os << "---";
+            }
+            else{
+                os << "   ";
+            }
+        }
+        os << "+";
+        //Output cells and walls in row
+        for (int c = 0; c < numCols; c++){
+            //Output left (west) wall
+            if (hasWall(r, c, Direction::WEST)){
+                os << "|";
+            }
+            else{
+                os << " ";
+            }
+            //Output cell start or end, else, empty
+            if (cell.getStart() == Location(r, c)){
+                os << " S ";
+            }
+            else if (cell.getStart() == Location(r, c)){
+                os << " E ";
+            }
+            else {
+                os << "   ";
+            }
+        }
+        if (hasWall(r, numCols-1, Direction::EAST)){
+            os << "|";
+        }
+        else{
+            os << " ";
+        }
+        os << endl;
+    }
+    //Output bottom wall
+    for (int c = 0; c < numCols; c++){
+        os << "+";
+        if (hasWall(numRows-1, c, Direction::SOUTH)){
+            os << "---";
+        }
+        else{
+            os << "   ";
+        }
+    }
+    os << "+";
+}
+
+// Take 2D expanded coordinates and compute the corresponding 1D array index
+int getArrayIndex(const Location &loc) const{
+    return (numCol*cellRow) + cellCol;
+}
+
+// Returns the expanded coordinates of the specified cell coordinates
+Location getCellArrayCoord(int cellRow, int cellCol) const{
+    return Location(cellRow*2 +1, cellCol*2+1);
+}
+
+// Returns the expanded coordinates of the wall on a specific side of
+// a cell given in cell coordinates
+Location getWallArrayCoord(int cellRow, int cellCol,
+                            Direction direction) const{
+    assert(cellRow >= 0 && cellRow < numRows);
+    assert(cellCol >= 0 && cellCol < numCols);
+    Location loc = getCellArrayCoord(cellRow, cellCol);
+    switch direction{
+        case Direction::NORTH:
+            loc.row--;
+            return loc;
+        case Direction::EAST:
+            loc.col++;
+            return loc;
+        case Direction::SOUTH:
+            loc.row++;
+            return loc;
+        case Direction::WEST:
+            loc.col--;
+            return loc;
+        default:
+            //Should never reach here
+            assert(false);
+    }
+}
