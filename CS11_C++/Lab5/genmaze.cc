662149b16a5146737b8ebfd4e55f232f7df33b88
Will Werst <wwerst@caltech.edu>
2017-05-23 12:56:13 -0700

    done with lab 5

--- CS11_C++/Lab5/genmaze.cc
+++ CS11_C++/Lab5/genmaze.cc
@@ -5,7 +5,7 @@
 #include "maze.hh"
 
 
-
+// Helper function to add a direction to v if the cell in that direction hasn't been visited
 void addDirectionOption(const Maze &maze, const Location &current,
                         Direction dir, vector<Direction> &v){
   Location nextCell = maze.getNeighborCell(current.row, current.col, dir);
@@ -36,23 +36,33 @@ int main(int argc, char *argv[]){
   int numRows = atoi(argv[1]);
   int numCols = atoi(argv[2]);
 
-
+  // Create maze
   Maze maze = Maze(numRows, numCols);
+  
+  // Create path vector for generating maze layout
   vector<Location> path = vector<Location>();
+
+  // Initialize maze
   maze.clear();
   maze.setAllWalls();
   maze.setStart(0, 0); // Set start top-left cell
   maze.setEnd(numRows-1, numCols-1); // Set end bottom-right cell
   maze.setVisited(maze.getStart().row, maze.getStart().col);
+
+  // Set start of path to start.
   path.push_back(maze.getStart());
+  // Generate maze by walking along paths until there are no paths with new directions to go.
   while (!path.empty()){
+    // Push the current location
     Location current = path.back();
     
-
+    // If we reached end, back up and keep going in other directions
     if (current == maze.getEnd()){
       path.pop_back();
       continue;
     }
+
+    // Generate a list of all the directions we could go
     vector<Direction> options = vector<Direction>();
     
 
@@ -69,11 +79,13 @@ int main(int argc, char *argv[]){
       addDirectionOption(maze, current, Direction::EAST, options);
     }
     
+    // If no directions to go, back up on path
     if (options.empty()){
       path.pop_back();
       continue;
     }
     
+    // Select random direction, and clear wall in that direction and add that move to path.
     int index = rand() % options.size();
     Direction dir = options[index];
     maze.clearWall(current.row, current.col, dir);
@@ -82,6 +94,7 @@ int main(int argc, char *argv[]){
     path.push_back(next);
     
   }
+  // print the maze
   maze.print(std::cout);
     
 }
