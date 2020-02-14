b5d2145a0196e08f274abc9c3df07af8db7a22d6
Will Werst <werstiv@gmail.com>
2020-02-14 05:29:04 -0800

    Shit works

--- CS11_Advanced_C++/Lab2/mbrot.cpp
+++ CS11_Advanced_C++/Lab2/mbrot.cpp
@@ -1,1 +1,19 @@
 #include "mbrot.h"
+
+MandelbrotPointInfo compute_mandelbrot(d_complex c, int max_iters,
+                                       bool collect_points) {
+	auto mbp_info = MandelbrotPointInfo {};
+	mbp_info.max_iters = max_iters;
+	size_t i = 0;
+	d_complex z {0, 0};
+	mbp_info.initial_point = z;
+	for (i = 0; i < max_iters && std::norm(z) < 4; i++) {
+		z = z*z + c;
+		if (collect_points) {
+			mbp_info.points_in_path.push_back(z);
+		}
+	}
+	mbp_info.num_iters = i;
+	mbp_info.escaped = (std::norm(z) > 4);
+	return mbp_info;
+}
