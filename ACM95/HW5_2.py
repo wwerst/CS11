85efb4a7e3c71fa0ff3da4f95a8da5c8cce49eb0
William Werst <werstiv@gmail.com>
2020-02-27 16:41:18 -0800

    Add HW5_2 ACM 95 problem test script

---
+++ ACM95/HW5_2.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from copy import deepcopy
+
+
+def iterate(t=1000, dt=0.001):
+    bug_pos = np.array(
+        [+0.5 + 0.5j,
+         -0.5 + 0.5j,
+         -0.5 - 0.5j,
+         +0.5 - 0.5j])
+         # dtype=np.clongdouble)
+    bug1_pos = []
+    for i in range(int(t/dt)):
+        bug_dt = np.roll(bug_pos, -1) - bug_pos
+        bug_pos += bug_dt * dt
+        bug1_pos.append(deepcopy(bug_pos[0]))
+    return bug1_pos
+
+
+def main():
+    points = iterate()
+    points = np.array(points)
+    angles = np.log(points).imag
+    delta_angles = np.diff(angles)
+    sum_angles = np.mod(delta_angles, 2*np.pi).sum()
+    print(sum_angles)
+    line_integral = np.abs(np.diff(points)).sum()
+    print(line_integral)
+    plt.plot(points.real, points.imag)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
